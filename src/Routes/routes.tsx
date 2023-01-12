272106af090ac47b7ed46acbcf6adafa050f5cca
MORSHED-HOME <morshed952640@gmail.com>
2023-01-13 00:16:58 +0600

    Error page added

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -5,11 +5,13 @@ import ContactUs from "../Pages/ContactUs/ContactUs";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
+import ErrorPage from "../Shared/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
     {
         path: '/', 
         element: <Root></Root>, 
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/', 
