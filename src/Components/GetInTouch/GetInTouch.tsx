6edbd1a94b15cbff708b9dded1160e55484ce74e
Achintya151 <achintyakumartalukdar@gmail.com>
2023-01-12 16:58:06 +0600

    get in touch

---
+++ src/Components/GetInTouch/GetInTouch.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GetInTouch = () => {
+    return (
+        <div>
+            <h1>Get In touch component</h1>
+        </div>
+    );
+};
+
+export default GetInTouch;
