82538056f8b9264359dda19dd48106fc4226e0ec
ZAMANXD <znsnlab@gmail.com>
2023-01-12 18:43:33 +0600

    daisyUI added

--- src/Components/HeroSection/Hero.tsx
+++ src/Components/HeroSection/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            
+            <h3 className='text-3xl'>This is hero section</h3>
         </div>
     );
 };
