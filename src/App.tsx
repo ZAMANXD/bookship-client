3f15095a6ab4542b92b999c2acaefaa672220b92
Achintya151 <achintyakumartalukdar@gmail.com>
2023-01-12 17:31:14 +0600

    get in touch section created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import GetInTouch from './Components/GetInTouch/GetInTouch';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-bold underline'>Book Ship</h1>
+      <GetInTouch></GetInTouch>
     </div>
   );
 }
--- src/Components/GetInTouch/GetInTouch.tsx
+++ src/Components/GetInTouch/GetInTouch.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 const GetInTouch = () => {
     return (
-        <div>
-            <h1>Get In touch component</h1>
+        <div className='bg-gradient-to-bl from-violet-600 to-indigo-500'>
+            <div className='md:block flex justify-center w-3/4 mx-auto p-20'>
+                <input className=' bg-slate-300 p-3 rounded-l' placeholder='Email' type="text" />
+                <input className='bg-orange-600 rounded-r p-3 text-white font-semibold' type="submit" value="Subscribe" />
+            </div>
         </div>
     );
 };
