1a62aefb9f47128f80920143def97ae5537c7cc1
Achintya151 <achintyakumartalukdar@gmail.com>
2023-01-12 17:44:40 +0600

    get in touch section  corner rounded

--- src/Components/GetInTouch/GetInTouch.tsx
+++ src/Components/GetInTouch/GetInTouch.tsx
@@ -4,8 +4,8 @@ const GetInTouch = () => {
     return (
         <div className='bg-gradient-to-bl from-violet-600 to-indigo-500'>
             <div className='md:block flex justify-center w-3/4 mx-auto p-20'>
-                <input className=' bg-slate-300 p-3 rounded-l' placeholder='Email' type="text" />
-                <input className='bg-orange-600 rounded-r p-3 text-white font-semibold' type="submit" value="Subscribe" />
+                <input className=' bg-slate-300 p-3 -mr-6 rounded-l-full shadow-xl focus:outline-0' placeholder='Email' type="text" />
+                <input className='bg-orange-600 rounded-full p-3 text-white font-semibold shadow-xl' type="submit" value="Subscribe" />
             </div>
         </div>
     );
