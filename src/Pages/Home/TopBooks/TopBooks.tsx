b793bfdfa1c45d3e082deb6914b0fd4be9290e76
MORSHED-HOME <morshed952640@gmail.com>
2023-01-13 16:10:38 +0600

    Our Top Books and New Releases section done!

--- src/Pages/Home/TopBooks/TopBooks.tsx
+++ src/Pages/Home/TopBooks/TopBooks.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import Book from './Book';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 const TopBooks = () => {
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
     return (
-        <div>
-            TopBooks
-        </div>
+            <div className='max-w-7xl mx-auto px-5 py-5'>
+                <h2 className="my-3 text-2xl md:text-3xl lg:text-5xl font-bold text-[#34315D] ml-5">Our Top Books</h2>
+                <div className='my-5'>
+                    <Carousel responsive={responsive}>
+                        {
+                            [...Array(6)].map((book, i) => <Book key={i}></Book>)
+                        }
+                    </Carousel>
+                </div>
+            </div>
     );
 };
 
