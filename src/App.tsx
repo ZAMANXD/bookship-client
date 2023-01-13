0e68661aa66838a17f9bcb5e71eab750eb03f169
Mostafizur <mostafizurdh2000@gmail.com>
2023-01-13 16:27:45 +0600

    add json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl font-bold underline'>Book Ship</h1>
+      <Footer/>
     </div>
   );
 }
--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,172 +1,169 @@
-import React from "react";
-import { Link } from "react-router-dom";
 
-const Footer = () => {
-  return (
-    <div>
-      <footer aria-label="Site Footer" class="bg-white">
-        <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+function Footer() {
+    return (
+        <div>
+            <footer aria-label="Site Footer" class="bg-white">
+                <div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
 
-          <div class="mt-16 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
-            <div class="text-center sm:text-left">
-              <p class="text-lg font-medium text-gray-900">About Us</p>
+                    <div class="mt-16 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+                        <div class="text-center sm:text-left">
+                            <p class="text-lg font-medium text-gray-900">About Us</p>
 
-              <nav aria-label="Footer About Nav" class="mt-8">
-                <ul class="space-y-4 text-sm">
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Company History
-                    </Link>
-                  </li>
+                            <nav aria-label="Footer About Nav" class="mt-8">
+                                <ul class="space-y-4 text-sm">
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            href="/"
+                                        >
+                                            Company History
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Meet the Team
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Meet the Team
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Employee Handbook
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Employee Handbook
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Careers
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Careers
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
 
-            <div class="text-center sm:text-left">
-              <p class="text-lg font-medium text-gray-900">Our Services</p>
+                        <div class="text-center sm:text-left">
+                            <p class="text-lg font-medium text-gray-900">Our Services</p>
 
-              <nav aria-label="Footer Services Nav" class="mt-8">
-                <ul class="space-y-4 text-sm">
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Web Development
-                    </Link>
-                  </li>
+                            <nav aria-label="Footer Services Nav" class="mt-8">
+                                <ul class="space-y-4 text-sm">
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Web Development
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Web Design
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Web Design
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Marketing
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Marketing
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Google Ads
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Google Ads
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
 
-            <div class="text-center sm:text-left">
-              <p class="text-lg font-medium text-gray-900">Resources</p>
+                        <div class="text-center sm:text-left">
+                            <p class="text-lg font-medium text-gray-900">Resources</p>
 
-              <nav aria-label="Footer Resources Nav" class="mt-8">
-                <ul class="space-y-4 text-sm">
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Online Guides
-                    </Link>
-                  </li>
+                            <nav aria-label="Footer Resources Nav" class="mt-8">
+                                <ul class="space-y-4 text-sm">
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Online Guides
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Conference Notes
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Conference Notes
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Forum
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Forum
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Downloads
-                    </Link>
-                  </li>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Downloads
+                                        </a>
+                                    </li>
 
-                  <li>
-                    <Link
-                      class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
-                      to="/"
-                    >
-                      Upcoming Events
-                    </Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
+                                    <li>
+                                        <a
+                                            class="text-[#46760A] transition text-base hover:text-[#46760A]/75"
+                                            to="/"
+                                        >
+                                            Upcoming Events
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
 
-          </div>
+                    </div>
 
 
-          <div class="mt-16 sm:flex sm:items-center sm:justify-between">
-            <h1 class="flex justify-center text-[#46760A] sm:justify-start font-bold text-2xl">
-              
-              Book <span className="text-black">Ship</span>
-            </h1>
+                    <div class="mt-16 sm:flex sm:items-center sm:justify-between">
+                        <h1 class="flex justify-center text-[#46760A] sm:justify-start font-bold text-2xl">
 
-            <p class="mt-4 text-center text-sm text-gray-500 sm:mt-0 sm:text-right">
-              Copyright &copy; 2022. All rights reserved.
-            </p>
-          </div>
+                            Book <span className="text-black">Ship</span>
+                        </h1>
+
+                        <p class="mt-4 text-center text-sm text-gray-500 sm:mt-0 sm:text-right">
+                            Copyright &copy; 2022. All rights reserved.
+                        </p>
+                    </div>
+                </div>
+            </footer>
         </div>
-      </footer>
+    )
+}
 
-    </div>
-  );
-};
-
-export default Footer;
+export default Footer
