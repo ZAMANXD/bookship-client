8329e88fb7415de2ad828854b2f69a9c672569e4
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-01-13 16:00:54 +0530

    complite the nav bar section

--- src/Pages/Home/NavBar/NavBar.tsx
+++ src/Pages/Home/NavBar/NavBar.tsx
@@ -1,11 +1,149 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
 
 const NavBar = () => {
-    return (
-        <div>
-            NavBar
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+  const navItem = <>
+  
+  <li>
+           
+           <div className="flex border rounded-full">
+           <input type="text" className="px-6 py-2 rounded-l-full" placeholder="Search a book..."/>
+               <button className="flex items-center justify-center px-4 border-l">
+                 <svg className="h-4 w-4 text-grey-dark" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"/></svg>
+               </button>
+           </div>
+            
+                       </li>
+                       <li>
+                         <Link
+                           to="/"
+                           aria-label="Our product"
+                           title="Our product"
+                           className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                         >
+                           Features
+                         </Link>
+                       </li>
+                    
+                       <li>
+                         <Link
+                           to="/"
+                           aria-label="About us"
+                           title="About us"
+                           className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
+                         >
+                           About us
+                         </Link>
+                       </li>
+<li>
+<FaShoppingCart/>
+</li>
+                       <li>
+                         <Link
+                           to="/"
+                           className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide transition duration-200 rounded-full bg-[#3DB188] text-white" 
+                           aria-label="Sign up"
+                           title="Sign up"
+                         >
+                          Log In
+                         </Link>
+                       </li>
+  </>
+
+  return (
+    <div className="bg-gray-50 ">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <a
+            href="/"
+            aria-label="Company"
+            title="Company"
+            className="inline-flex items-center"
+          >
+            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+              BookShip
+            </span>
+          </a>
+          <ul className="flex items-center hidden space-x-8 lg:flex">
+           {
+            navItem
+           }
+          </ul>
+          <div className="lg:hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
+                />
+                <path
+                  fill="currentColor"
+                  d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
+                />
+              </svg>
+            </button>
+            {isMenuOpen && (
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-gray-50 border rounded shadow-2xl">
+                  <div className="flex items-center justify-between mb-4">
+                    <div>
+                      <a
+                        href="/"
+                        aria-label="Company"
+                        title="Company"
+                        className="inline-flex items-center"
+                      >
+                       
+                        <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                          BookShip
+                        </span>
+                      </a>
+                    </div>
+                    <div>
+                      <button
+                        aria-label="Close Menu"
+                        title="Close Menu"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <svg className="w-5 text-gray-600" viewBox="0 0 24 24">
+                          <path
+                            fill="currentColor"
+                            d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <nav>
+                    <ul className="space-y-4">
+                   {
+                    navItem
+                   }
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
