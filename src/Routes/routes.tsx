3d1f6ce11fef622ee914f4dd062e064dad82edbe
MORSHED-HOME <morshed952640@gmail.com>
2023-01-16 01:38:06 +0600

    Book details added

--- src/Routes/routes.tsx
+++ src/Routes/routes.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root/Root";
 import AboutUs from "../Pages/AboutUs/AboutUs";
+import AllBooks from "../Pages/AllBooks/AllBooks";
+import BookDetails from "../Pages/BookDetails/BookDetails";
 import ContactUs from "../Pages/ContactUs/ContactUs";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
@@ -9,30 +11,41 @@ import ErrorPage from "../Shared/ErrorPage/ErrorPage";
 
 export const router = createBrowserRouter([
     {
-        path: '/', 
-        element: <Root></Root>, 
+        path: '/',
+        element: <Root></Root>,
         errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
-                path: '/', 
-                element: <Home/>
-            }, 
+                path: '/',
+                element: <Home />
+            },
             {
-                path: '/aboutus', 
-                element: <AboutUs/>
-            }, 
+                path: '/aboutus',
+                element: <AboutUs />
+            },
             {
-                path: '/contactus', 
-                element: <ContactUs/>
-            }, 
+                path: '/contactus',
+                element: <ContactUs />
+            },
             {
-                path: '/login', 
-                element: <Login/>
-            }, 
+                path: '/login',
+                element: <Login />
+            },
             {
-                path: '/signup', 
-                element: <SignUp/>
-            }, 
+                path: '/signup',
+                element: <SignUp />
+            },
+            {
+                path: '/book/:id',
+                loader: async ({ params }) => {
+                    return fetch(`/book/${params.id}`)
+                },
+                element: <BookDetails />
+            },
+            {
+                path: '/books',
+                element: <AllBooks />,
+            },
         ]
     }
 ])
