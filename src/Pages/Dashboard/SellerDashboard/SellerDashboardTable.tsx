941e26476920a276d45eba2513e9659ea935752a
Mostafizur <mostafizurdh2000@gmail.com>
2023-01-17 23:59:23 +0600

    seller add book image

--- src/Pages/Dashboard/SellerDashboard/SellerDashboardTable.tsx
+++ src/Pages/Dashboard/SellerDashboard/SellerDashboardTable.tsx
@@ -8,19 +8,17 @@ const SellerDashboardTable = () => {
                 <div className=" mt-7">
                     <div className="w-full text-xs text-left ">
                         <div>
-                            <div className="bg-gray-300 hidden  lg:grid grid-cols-[20%_10%_15%_20%_15%_20%]">
-                                <div className="p-3">Product Name</div>
+                            <div className="bg-gray-300 hidden  lg:grid grid-cols-[40%_20%_20%_20%]">
+                                <div className="p-3">Book Title</div>
                                 <div className="p-3">Price</div>
-                                <div className="p-3">Date</div>
-                                <div className="p-3">Email</div>
-                                <div className="p-3">Phone</div>
+                                <div className="p-3">Discount Price</div>
                                 <div className="p-3">Acction</div>
                             </div>
                         </div>
                         <div className="border-b  border-gray-700 ">
-                            <div className="grid lg:grid-cols-[20%_10%_15%_20%_15%_20%] md:grid-cols-[1fr_1fr_1fr] break-words">
+                            <div className="grid lg:grid-cols-[40%_20%_20%_20%] md:grid-cols-[1fr_1fr_1fr] break-words">
                                 <div className="px-3 py-2">
-                                    <p className='lg:hidden font-semibold text-base'>Product Name: </p>
+                                    <p className='lg:hidden font-semibold text-base'>Book Title: </p>
                                     <p>{'name'}</p>
                                 </div>
                                 <div className="px-3 py-2">
@@ -28,17 +26,17 @@ const SellerDashboardTable = () => {
                                     <p>{'sellarPrice'}</p>
                                 </div>
                                 <div className="px-3 py-2">
-                                    <p className='lg:hidden font-semibold text-base'>Buyer Name: </p>
+                                    <p className='lg:hidden font-semibold text-base'>Discount Price: </p>
                                     <p>{'date'}</p>
                                 </div>
-                                <div className="px-3 py-2">
+                                {/* <div className="px-3 py-2">
                                     <p className='lg:hidden font-semibold text-base'>Email: </p>
                                     <p>{'userEmail'}</p>
                                 </div>
                                 <div className="px-3 py-2">
                                     <p className='lg:hidden font-semibold text-base'>Phone: </p>
                                     <p>{'phoneNumber'}</p>
-                                </div>
+                                </div> */}
                                 <div className="px-3 py-2 ">
                                     <p className='lg:hidden font-semibold text-base'>Action: </p>
                                     <button className="btn bg-green-600">Delet</button>
