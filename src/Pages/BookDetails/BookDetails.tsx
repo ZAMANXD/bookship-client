ef84dc7d12db064486d781a00406108f8c1e53ff
MORSHED-HOME <morshed952640@gmail.com>
2023-01-20 16:29:29 +0600

    Book property name updated

--- src/Pages/BookDetails/BookDetails.tsx
+++ src/Pages/BookDetails/BookDetails.tsx
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 
 const BookDetails = () => {
     let book: any = useLoaderData()
-    const { bookImage, detailes, discountPrice, price, sellerName, sellerPhoto, title } = book
+    const { _id, bookTitle, authorName, authorEmail, authorImg, authorRating, bookRating, originalPrice, discountedPrice, bookCover, description, category, publication } = book
 
     console.log(book.title);
     return (
@@ -12,21 +12,28 @@ const BookDetails = () => {
             <h2 className='text-5xl font-semibold text-[#34315D]'>Book Details</h2>
             <div className='grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-10 py-5'>
                 <div>
-                    <img className='w-full' src={bookImage} alt="" />
+                    <img className='w-full' src={bookCover} alt="" />
                 </div>
                 <div>
-                    <h3 className='text-[#34315D] text-4xl font-semibold'>{title}</h3>
-                    <p className='my-5'>Description: {detailes}</p>
-                    <p className='my-5'>Author: {sellerName}</p>
+                    <h3 className='text-[#34315D] text-4xl font-semibold'>{bookTitle}</h3>
+                    <p className='my-3'>Description: {description}</p>
+                    <p className='my-3'>Author: {authorName}</p>
                     <div className='flex gap-x-2 items-center'>
                         Author Ratings: {
-                            [...Array((5))].map((star, i) => <AiOutlineStar key={i} className='text-lg text-yellow-500' />)
+                            [...Array((parseInt(authorRating || 4)))].map((star, i) => <AiOutlineStar key={i} className='text-lg text-yellow-500' />)
+                        }
+                    </div>
+                    <p className='my-3'>Category: {category}</p>
+                    <p className='my-3'>Publication: {publication}</p>
+                    <div className='flex gap-x-2 items-center'>
+                        Book Ratings: {
+                            [...Array((parseInt(bookRating || 4)))].map((star, i) => <AiOutlineStar key={i} className='text-lg text-yellow-500' />)
                         }
                     </div>
                     <div className='flex items-end gap-x-10 my-10'>
                         <div>
-                            <p className='text-sm font-semibold text-gray-500 '>$ <span className='line-through text-xl'>{price}</span></p>
-                            <p className='text-4xl font-bold text-[#342D65]'>$ {discountPrice}</p>
+                            <p className='text-sm font-semibold text-gray-500 '>$ <span className='line-through text-xl'>{originalPrice}</span></p>
+                            <p className='text-4xl font-bold text-[#342D65]'>$ {discountedPrice}</p>
                         </div>
 
                         <button className='px-3 h-10 bg-[#3DB188] rounded-md text-white'>Add to Cart</button>
