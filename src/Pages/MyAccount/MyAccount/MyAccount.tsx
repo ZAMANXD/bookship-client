dab98d88dc61ddfd33235654dff825205534e0d7
TarekSolaiman <mdtareksolaiman16@gmail.com>
2023-02-03 18:11:27 +0600

    My Account section is ready

--- src/Pages/MyAccount/MyAccount/MyAccount.tsx
+++ src/Pages/MyAccount/MyAccount/MyAccount.tsx
@@ -1,39 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { HiPlusSm } from 'react-icons/hi';
+import { AuthContext } from '../../../context/AuthProvider';
+import avatar from "../../../imgFile/avatar.png"
 
 const MyAccount = () => {
+    const {user}=useContext(AuthContext)
     return (
         <div className='md:w-1/2 mx-auto bg-gray-50 md:p-20 p-10 md:my-8 shadow-md hover:shadow-2xl'>
-            <h1 className='text-xl font-semibold mb-4'>Edit Profile</h1>
-            <div className='flex flex-col justify-center space-y-6'>
-                <div >
+            <h1 className='text-xl text-center font-semibold mb-4'>Edit Profile</h1>
+            <div className='flex flex-col justify-center space-y-6 p-3'>
+                <div>
                 <label htmlFor="upload-button" className='relative ' >
-                <img className='h-20 mx-auto border-[#3DB188] border-4 rounded-full hover:shadow-lg' src="https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png" alt="" />
+                <img className='h-20 mx-auto border-[#3DB188] border-4 rounded-full hover:shadow-lg' src=
+                {user?.photoURL?user.photoURL:avatar} alt="" />
                 <div className='absolute bottom-0 ml-10 left-60 text-3xl text-white font-bold bg-[#3DB188] rounded-full hover:shadow-lg'><HiPlusSm /></div>
                 </label>
                 </div>
-                <input type="file" name="" className='hidden' id="upload-button" />
                 <div className='flex lg:flex-row flex-col lg:space-y-0 space-y-6 justify-between'>
-                    <div className='flex flex-col'>
-                        <label className='font-semibold pb-4'>First Name</label>
-                        <input className='border hover:shadow-lg' type="text" />
-                    </div>
-                    <div className='flex flex-col'>
-                        <label className='font-semibold pb-4'>Last Name</label>
-                        <input className='border hover:shadow-lg' type="text" />
+                    <div className='w-full flex justify-between items-center'>
+                        <label className='font-semibold'>Name</label>
+                        <input className='border-b-2 border-gray-500 w-[75%] ml-3 text-lg font-medium' defaultValue={user?.displayName} type="text" />
                     </div>
                 </div>
-                <div className='flex flex-col'>
-                    <label className='font-semibold pb-4'>Email</label>
-                    <input className='border hover:shadow-lg' type="email" />
+                <div className='w-full flex justify-between items-center'>
+                    <label className='font-semibold'>Email</label>
+                    <input className='border-b-2 border-gray-500 w-[75%] ml-3 text-lg font-medium'
+                     value={user?.email}
+                     readOnly
+                     type="email" />
                 </div>
-                <div className='flex flex-col'>
-                    <label className='font-semibold pb-4'>Address</label>
-                    <input className='border hover:shadow-lg' type="text" />
+                <div className='w-full flex justify-between items-center'>
+                    <label className='font-semibold'>Address</label>
+                    <input className='border-b-2 border-gray-500 w-[75%] ml-3 text-lg font-medium' type="text" />
                 </div>
-                <div className='flex flex-col'>
-                    <label className='font-semibold pb-4'>Password</label>
-                    <input className='border hover:shadow-lg' type="password" />
+                <div className='w-full flex justify-between items-center'>
+                    <label className='font-semibold'>Password</label>
+                    <input className='border-b-2 border-gray-500 w-[75%] ml-3 text-lg font-medium' type="password" />
                 </div>
                 <input className='bg-[#3DB188] border hover:border-[#3DB188] hover:bg-white hover:text-[#3DB188] py-2 px-20 text-white font-semibold rounded-full mx-auto' type="submit" value="Save" />
             </div>
