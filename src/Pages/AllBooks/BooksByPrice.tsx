c966f32e56026dab30f5f14974c381f16191cd4d
MORSHED-HOME <morshed952640@gmail.com>
2023-02-06 22:57:20 +0600

    Price low to high and high to low done

--- src/Pages/AllBooks/BooksByPrice.tsx
+++ src/Pages/AllBooks/BooksByPrice.tsx
@@ -2,29 +2,36 @@ import React, { useEffect, useState } from 'react';
 import useTitle from '../../hooks/useTitle';
 import SingleBook from '../../Shared/SingleBook/SingleBook';
 import Spinner from '../../Shared/Spinner/Spinner';
+import { BsArrowLeft, BsArrowRight } from 'react-icons/bs'
 
 const BooksByPrice = () => {
+    const [value, setValue] = useState(1)
     const [books, setBooks] = useState([])
     const [loading, setLoading] = useState(false)
 
     useTitle(`- Low to High`)
     useEffect(() => {
         setLoading(true)
-        fetch("https://bookship-server-zamanxd.vercel.app/books")
+        fetch(`http://localhost:5000/booksprice?value=${value}`)
             .then((res) => res.json())
             .then((data) => {
                 setBooks(data)
                 setLoading(false)
             });
-    }, [])
-
+    }, [value])
+    console.log(value);
     if (loading) {
         return <Spinner />
     }
 
     return (
-        <div className='py-10 max-w-7xl mx-auto'>
-            <h2 className='text-center text-3xl my-5'> Low to High :<span className='uppercase'>{books.length}</span></h2>
+        <div className='py-5 max-w-7xl mx-auto'>
+            <div className='flex justify-between items-center p-5'>
+                <button onClick={() => setValue(1)} className='border rounded-md p-2 flex items-center gap-x-2 text-gray-600'>Low <BsArrowRight /> High</button>
+                <span className='lg:text-2xl font-semibold'>Books: <span>{books?.length}</span></span>
+                <button onClick={() => setValue(-1)} className='border rounded-md p-2 flex items-center gap-x-2 text-gray-600'>High <BsArrowLeft /> Low</button>
+
+            </div>
             <div className='grid grid-cols-1 md:grid-cols-2 px-5 lg:grid-cols-3 gap-5 pb-10'>
                 {
                     books.map((book: any, i: number) => <SingleBook key={i} {...book} />)
