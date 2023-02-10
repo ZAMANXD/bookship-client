2b5c2c54b7b15d34ef66bac3daf4c299645c06f9
Mostafizur <mostafizurdh2000@gmail.com>
2023-02-10 08:56:52 +0600

    when post form data added date

--- src/Pages/Dashboard/AdminDashboard/AdminDashboard.tsx
+++ src/Pages/Dashboard/AdminDashboard/AdminDashboard.tsx
@@ -1,16 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
+import BookPostForm from "../../../Shared/Comments/BookPostForm";
 
 const AdminDashboard = () => {
-  let count:any = 1;
-  useEffect(()=>{
-    const url = window.location.href;
-    const number = url.replace(url, '1')
-    
-    count = count + number
-  }, [])
+  const { data: ordersCollection, refetch } = useQuery({
+    queryKey: ["orders"],
+    queryFn: async () => {
+      const res = await fetch(
+        `https://bookship-server-zamanxd.vercel.app/orders`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+  const ordersBooks: any = JSON.parse(localStorage.getItem("cart") as any);
+ 
+  const initialValue = 0;
+  const totalOrderBooks = ordersBooks.reduce(
+    (accumulator:any, book:any) => accumulator + book.quantity,
+    initialValue
+  );
+  console.log(totalOrderBooks)
 
-
-  return <div className=""></div>;
+  const url = window.location.href
+  console.log(url === "http://localhost:3000/")
+  return (
+    <div className="">
+      <BookPostForm />
+    </div>
+  );
 };
 
 export default AdminDashboard;
