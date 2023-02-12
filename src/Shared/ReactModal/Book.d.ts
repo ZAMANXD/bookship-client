f42d53fbb389145d6c2d8e9e89a772d167407d89
ZAMANXD <znsnlab@gmail.com>
2023-02-12 22:49:21 +0600

    search functionality fixed

---
+++ src/Shared/ReactModal/Book.d.ts
@@ -0,0 +1,11 @@
+// Book.d.ts
+export interface Book {
+    id: string;
+    title: string;
+    authors: string[];
+    coverImage: string;
+    rating: number;
+    price: string;
+    link: string;
+  }
+  
--- src/Shared/ReactModal/ReactModal.tsx
+++ src/Shared/ReactModal/ReactModal.tsx
@@ -2,22 +2,25 @@ import React from "react";
 import Modal from "react-modal";
 import Book from "../../Pages/Home/TopBooks/Book";
 import { HiOutlineX } from "react-icons/hi";
+import { Book as BookType } from "./Book";
+
+interface ReactModalProps {
+  modalIsOpen: boolean;
+  setIsOpen: (state: boolean) => void;
+  books?: any[];
+}
 
 const customStyles = {
   content: {
     top: "30%",
     left: "50%",
-    // right: "auto",
-    // bottom: "auto",
-    // marginRight: "-50%",
     transform: "translate(-50%, -30%)",
     zIndex: 99999,
-    width: '400px',
-    // backgroundColor: 'transparent'
+    width: "400px",
   },
 };
 
-const ReactModal = ({modalIsOpen, setIsOpen, book }: any) => {
+const ReactModal: React.FC<ReactModalProps> = ({ modalIsOpen, setIsOpen, books }) => {
   function closeModal() {
     setIsOpen(false);
   }
@@ -31,8 +34,12 @@ const ReactModal = ({modalIsOpen, setIsOpen, book }: any) => {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <button onClick={closeModal}><HiOutlineX className="text-3xl hover:text-red-600"/></button>
-        <Book {...book}/>
+        <button onClick={closeModal}>
+          <HiOutlineX className="text-3xl hover:text-red-600" />
+        </button>
+        {books?.map((book: BookType) => (
+          <Book key={book.id} {...book} />
+        ))}
       </Modal>
     </div>
   );
