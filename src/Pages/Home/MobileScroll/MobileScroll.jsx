41162621ec5debe68aa31e2c084c7f5da91022a6
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-02-17 18:32:11 +0530

    full design polish

--- src/Pages/Home/MobileScroll/MobileScroll.jsx
+++ src/Pages/Home/MobileScroll/MobileScroll.jsx
@@ -4,28 +4,28 @@ import ScreenText from "./ScreenText";
 
 const scrollData = [
   {
-    heading: "we’ve got your back.",
+    heading: "Good User Friendly",
     description:
-      "gain complete control over your credit card with CRED Protect. receive category-based analysis of your spends, detect hidden charges, and track your credit limit in real-time.",
+      "Experience hassle-free browsing with our user-friendly website. With its clear layout, intuitive design, and helpful features, users can easily navigate and find the information they need. Our site's responsive interface adapts to different devices, providing a seamless experience across all platforms. From quick page load times to easy-to-find menus, our website ensures an efficient and enjoyable experience for all users.",
     mobile_img: "https://i.ibb.co/5KDvwYY/2.jpg",
   },
   {
-    heading: "begin your winning streak.",
+    heading: "Get Best Price",
     description:
-      "use your CRED coins to participate in games and raffles to win the most exclusive rewards and cashbacks on CRED. good luck.",
+      "Get the best deals with our unbeatable prices. We offer competitive pricing without compromising on quality, ensuring our customers get the best value for their money. Our price match guarantee ensures you get the lowest price available. Whether you're looking for products or services, we strive to provide the best possible pricing options to meet your needs and budget.",
     mobile_img: "https://i.ibb.co/4pSxdzR/3.jpg",
   },
   {
-    heading: "for your eclectic taste.",
+    heading: "Very Easy to Pay ",
     description:
-      "get access to the CRED Store, a member-exclusive selection of products and experiences at special prices that complement your taste. this is the good life they speak of.",
-    mobile_img: "https://i.ibb.co/CQ7t9x9/4.jpg",
+      "Experience hassle-free payments with our easy-to-use Stripe payment gateway. We offer a secure and convenient payment method using Stripe, ensuring that our customers can easily and quickly make payments without any hassle. Our payment system is designed to provide a seamless and secure transaction process. Say goodbye to payment worries and enjoy a stress-free checkout experience with our hassle-free Stripe payment gateway.",
+    mobile_img: "https://i.ibb.co/V9r5JY5/1.jpg",
   },
   {
-    heading: "more cash in your pockets.",
+    heading: "Great Support",
     description:
-      "switch to CRED RentPay and start paying rent with your credit card. this way you get up to 45 days of credit free period, more reward points and a happy landlord.",
-    mobile_img: "https://i.ibb.co/V9r5JY5/1.jpg",
+      "Experience exceptional customer support with our dedicated team. We provide timely and effective solutions to our customers' queries and concerns, ensuring that they have a hassle-free experience. Our knowledgeable support team is available to assist you with any issues or questions. From live chat to email support, we are committed to providing the best possible customer service to meet your needs.",
+    mobile_img: "https://i.ibb.co/CQ7t9x9/4.jpg",
   },
 ];
 
