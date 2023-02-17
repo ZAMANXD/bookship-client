00cf808b6bc9305d6be031da218853ee0916f59c
Arghya20 <arghyaghosh.vlog@gmail.com>
2023-02-17 11:58:54 +0530

    minnor change

--- src/Pages/BookDetails/BookDetails.tsx
+++ src/Pages/BookDetails/BookDetails.tsx
@@ -1,71 +1,107 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react';
-import { AiOutlineStar } from 'react-icons/ai';
-import { useLoaderData } from 'react-router-dom';
-import { AuthContext } from '../../context/AuthProvider';
-import { useCart } from '../../context/CartContext';
-import useTitle from '../../hooks/useTitle';
-import CommentBox from '../../Shared/Comments/CommentBox';
-import Comments from '../../Shared/Comments/Comments';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../context/AuthProvider";
+import { useCart } from "../../context/CartContext";
+import useTitle from "../../hooks/useTitle";
+import CommentBox from "../../Shared/Comments/CommentBox";
+import Comments from "../../Shared/Comments/Comments";
 
 const BookDetails = () => {
-    const { user } = React.useContext(AuthContext)
-    const { increaseCartQuantity } = useCart();
+  const { user } = React.useContext(AuthContext);
+  const { increaseCartQuantity } = useCart();
 
-    let book: any = useLoaderData()
-    const { _id, bookTitle, authorName, authorRating, bookRating, originalPrice, discountedPrice, bookCover, description, category, publication } = book
-    useTitle(`- ${bookTitle}`)
+  let book: any = useLoaderData();
+  const {
+    _id,
+    bookTitle,
+    authorName,
+    authorRating,
+    bookRating,
+    originalPrice,
+    discountedPrice,
+    bookCover,
+    description,
+    category,
+    publication,
+  } = book;
+  useTitle(`- ${bookTitle}`);
 
-    const { data: comments, refetch } = useQuery({
-        queryKey: ['comments'],
-        queryFn: () => {
-            return fetch(`https://bookship-server-zamanxd.vercel.app/reviews/${book?._id}`)
-                .then(res => res.json())
-        }
-    })
+  const { data: comments, refetch } = useQuery({
+    queryKey: ["comments"],
+    queryFn: () => {
+      return fetch(
+        `https://bookship-server-zamanxd.vercel.app/reviews/${book?._id}`
+      ).then((res) => res.json());
+    },
+  });
 
-    return (
-        <div className='max-w-7xl mx-auto p-5 lg:p-10'>
-            <h2 className='text-3xl lg:text-5xl font-semibold text-[#34315D]'>Book Details</h2>
-            <div className='grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-10 py-5'>
-                <div>
-                    <img className='w-full' src={bookCover} alt="" />
-                </div>
-                <div>
-                    <h3 className='text-[#34315D] text-2xl lg:text-4xl font-semibold'>{bookTitle}</h3>
-                    <p className='my-3'><span className='font-semibold'>Description:</span> {description}</p>
-                    <div className='flex gap-x-2 items-center'>
-                        <span className='font-semibold'>Book Ratings:</span> {
-                            [...Array((parseInt(bookRating || 4)))].map((star, i) => <AiOutlineStar key={i} className='text-lg text-yellow-500' />)
-                        }
-                    </div>
-                    <div className='my-8'>
-                        <p className='my-3'><span className='font-semibold'>Author:</span> {authorName}</p>
-                        <div className='flex gap-x-2 items-center'>
-                            <span className='font-semibold'>Author Ratings:</span> {
-                                [...Array((parseInt(authorRating || 4)))].map((star, i) => <AiOutlineStar key={i} className='text-lg text-yellow-500' />)
-                            }
-                        </div>
-                    </div>
-                    <p className='my-3'><span className='font-semibold'>Category:</span> {category}</p>
-                    <p className='my-3'><span className='font-semibold'>Publication:</span> {publication}</p>
-
-                    <div className='flex items-end gap-x-10 my-10'>
-                        <div>
-                            <p className='text-sm font-semibold text-gray-500 '>$ <span className='line-through text-xl'>{originalPrice}</span></p>
-                            <p className='text-4xl font-bold text-[#342D65]'>$ {discountedPrice}</p>
-                        </div>
-
-                        <button onClick={() => increaseCartQuantity(_id)} className='px-3 h-10 bg-[#3DB188] rounded-md text-white'>Add to Cart</button>
-                    </div>
-                </div>
-            </div>
-            <div className='grid lg:grid-cols-[1fr_2fr] lg:gap-x-5'>
-                <CommentBox {...book} refetch={refetch} />
-                <Comments {...book} comments={comments} refetch={refetch} />
-            </div>
+  return (
+    <div className="max-w-7xl mx-auto p-5 lg:p-10">
+      <h2 className="text-3xl lg:text-4xl font-semibold text-[#34315D] mb-10">
+        Book Details
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-10 py-5 bg-gray-50 px-10 rounded-lg shadow">
+        <div className="w-10/12 rounded-lg  mx-auto">
+          <img className="rounded shadow-lg" src={bookCover} alt="" />
         </div>
-    );
+        <div>
+          <h3 className="text-[#34315D] text-3xl lg:text-4xl font-bold mb-8">
+            {bookTitle}
+          </h3>
+          <p className="my-3">
+            <span className="font-semibold">Description:</span> {description}
+          </p>
+          {/* <div className="flex gap-x-2 items-center">
+            <span className="font-semibold">Book Ratings:</span>{" "}
+            {[...Array(parseInt(bookRating || 4))].map((star, i) => (
+              <AiOutlineStar key={i} className="text-lg text-yellow-500" />
+            ))}
+          </div> */}
+          <div className="my-8">
+            <p className="my-3">
+              <span className="font-semibold">Author:</span> {authorName}
+            </p>
+            {/* <div className="flex gap-x-2 items-center">
+              <span className="font-semibold">Author Ratings:</span>{" "}
+              {[...Array(parseInt(authorRating || 4))].map((star, i) => (
+                <AiOutlineStar key={i} className="text-lg text-yellow-500" />
+              ))}
+            </div> */}
+          </div>
+          <p className="my-3">
+            <span className="font-semibold">Category:</span> {category}
+          </p>
+          <p className="my-3">
+            <span className="font-semibold">Publication:</span> {publication}
+          </p>
+
+          <div className="flex items-end gap-x-10 my-10">
+            <div>
+              <p className="text-sm font-semibold text-gray-500 ">
+                $ <span className="line-through text-xl">{originalPrice}</span>
+              </p>
+              <p className="text-4xl font-bold text-[#342D65]">
+                $ {discountedPrice}
+              </p>
+            </div>
+
+            <button
+              onClick={() => increaseCartQuantity(_id)}
+              className="px-3 h-10 bg-[#3DB188] rounded-md text-white"
+            >
+              Add to Cart
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="grid lg:grid-cols-[1fr_2fr] lg:gap-x-5 mt-20">
+        <CommentBox {...book} refetch={refetch} />
+        <Comments {...book} comments={comments} refetch={refetch} />
+      </div>
+    </div>
+  );
 };
 
 export default BookDetails;
--- src/Pages/Home/TopBooks/Book.tsx
+++ src/Pages/Home/TopBooks/Book.tsx
@@ -1,72 +1,91 @@
-import React, { useContext } from 'react';
-import { toast } from 'react-hot-toast';
-import { AiFillStar } from 'react-icons/ai'
-import { BsSuitHeart } from 'react-icons/bs';
-import { HiOutlineShoppingCart } from 'react-icons/hi';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../context/AuthProvider';
-import { useCart } from '../../../context/CartContext';
-import "./Book.css"
-
-
-
+import React, { useContext } from "react";
+import { toast } from "react-hot-toast";
+import { AiFillStar } from "react-icons/ai";
+import { BsSuitHeart } from "react-icons/bs";
+import { HiOutlineShoppingCart } from "react-icons/hi";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthProvider";
+import { useCart } from "../../../context/CartContext";
+import "./Book.css";
 
 const Book = ({ ...book }) => {
-    const {user}=useContext(AuthContext)
-    const { _id, bookTitle, authorName, authorEmail, authorImg, authorRating, bookRating, originalPrice, discountedPrice, bookCover, description, category, publication } = book
-    const { increaseCartQuantity } = useCart()
+  const { user } = useContext(AuthContext);
+  const {
+    _id,
+    bookTitle,
+    authorName,
+    authorEmail,
+    authorImg,
+    authorRating,
+    bookRating,
+    originalPrice,
+    discountedPrice,
+    bookCover,
+    description,
+    category,
+    publication,
+  } = book;
+  const { increaseCartQuantity } = useCart();
 
-    // Add to favourite
-    const favouritehandler = (fvitem:object,userEmail:string)=>{
-        const favouriteItem={...fvitem,userEmail}
-        console.log(favouriteItem);
-        fetch("https://bookship-server-zamanxd.vercel.app/favorurite",{
-                method:"PUT",
-                headers:{
-                    "content-type": "application/json"
-                },
-                body:JSON.stringify(favouriteItem)
-            })
-            .then(res=>res.json())
-            .then(data=>{
-                if(data.message){
-                    toast.error(data.message)
-                    console.log(data)
-                }else{
-                    toast.success('Added Favourite Section')
-                }
-            }
-            )
-            .catch(e=>console.log(e.message))
-    }
-    return (
-    <div className="card">
-        <div className="card-img">
-            <Link to={`/book/${_id}`}>
-                <img className=' w-full h-full rounded-lg' src={bookCover} alt="" />
-            </Link>
+  // Add to favourite
+  const favouritehandler = (fvitem: object, userEmail: string) => {
+    const favouriteItem = { ...fvitem, userEmail };
+    console.log(favouriteItem);
+    fetch("https://bookship-server-zamanxd.vercel.app/favorurite", {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(favouriteItem),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+          toast.error(data.message);
+          console.log(data);
+        } else {
+          toast.success("Added Favourite Section");
+        }
+      })
+      .catch((e) => console.log(e.message));
+  };
+  return (
+    <div className="book-card bs-box-shadow">
+      <div className="card-img">
+        <Link to={`/book/${_id}`}>
+          <img className=" w-full h-full rounded-lg" src={bookCover} alt="" />
+        </Link>
+      </div>
+      <div className="card-info">
+        <p className="text-title text-gray-900">
+          {bookTitle.length > 15 ? bookTitle.slice(0, 15) : bookTitle}
+        </p>
+        <div className="flex justify-start gap-x-2 my-2">
+          <img className="w-6 h-6 rounded-full border" src={authorImg} alt="" />
+          <p className="text-body text-center text-gray-900">
+            {authorName.length > 20 ? authorName.slice(0, 15) : authorName}
+          </p>
         </div>
-        <div className="card-info">
-            <p className="text-title text-gray-900">{bookTitle.length > 15 ? bookTitle.slice(0, 15) : bookTitle}</p>
-            <div className='flex justify-start gap-x-2 my-2'>
-                <img className='w-6 h-6 rounded-full border' src={authorImg} alt="" />
-                <p className="text-body text-center text-gray-900">{authorName.length> 20 ? authorName.slice(0,15):authorName}</p>
-            </div>
+      </div>
+      <div className="card-footer">
+        <span className="text-title text-gray-900">$ {discountedPrice}</span>
+        <div className="flex justify-end text-lg">
+          <div
+            className="card-button"
+            onClick={() => increaseCartQuantity(_id)}
+          >
+            <HiOutlineShoppingCart />
+          </div>
+          <div
+            className="card-button2 ml-3"
+            onClick={() => favouritehandler(book, user.email)}
+          >
+            <BsSuitHeart />
+          </div>
         </div>
-        <div className="card-footer">
-            <span className="text-title text-gray-900">$ {discountedPrice}</span>
-            <div className='flex justify-end text-lg'>
-                <div className="card-button" onClick={() => increaseCartQuantity(_id)}>
-                    <HiOutlineShoppingCart/>
-                </div>
-                <div className="card-button2 ml-3" 
-                    onClick={()=>favouritehandler(book,user.email)}>
-                    <BsSuitHeart/>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    );
+  );
 };
 
 export default Book;
