d3e9f4ff8df86152ad02a9925e4d5bc2620fe7da
ZAMANXD <znsnlab@gmail.com>
2023-02-25 21:03:04 +0600

    changed to old add to cart version

--- src/Pages/Cart/AddToCart/AddToCart.tsx
+++ src/Pages/Cart/AddToCart/AddToCart.tsx
@@ -1,82 +1,21 @@
-import {useContext} from 'react';
+import React from 'react';
 import { useCart } from '../../../context/CartContext';
 import ProductDetails from '../ProductDetails/ProductDetails';
 import Summary from '../Summary/Summary';
-import EmptyCartLotte from '../../../Lottie/EmptyCartLotte.json';
-import Lottie from "lottie-react";
-import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { AuthContext } from '../../../context/AuthProvider';
 
 const AddToCart = () => {
-    const { cartQuantity, cartItems } = useCart();
-    const {user}=useContext(AuthContext);
-
-    // const { } = useQuery({
-    //     queryKey:['cart'], 
-    // queryFn: async () => {
-    //     const response = await fetch('/cart');
-    //     if (!response.ok) {
-    //       throw new Error('Network response was not ok');
-    //     }
-        
-    //     const data = await response.json();
-    //     return data;
-    //   });
-
-      const { data, isLoading, isError } = useQuery({
-        queryKey: ['cart', user?.email],
-        queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/cart/achintyakumartalukdar@gmail.com`);
-            const data = await res.json();
-            return data;
-        }
-    })
-    
-      if (isLoading) {
-        return <div>Loading...</div>;
-      }
-    
-      if (isError) {
-        return <div>Error fetching cart data</div>;
-      }
-
-      console.log(data.items);
-      
-    
+    const { cartQuantity, cartItems } = useCart()
 
     return (
-        <>
-             {
-                    cartQuantity === 0 ?
-                    <div className='mx-auto max-w-screen-sm text-center'>
-                        <h1 className='mt-4 lg:text-4xl md:text-xl text-lg font-bold text-gray-900'>Your cart is empty</h1>
-                        <Lottie className='lg:h-[30rem] md:h-[20rem]  h-32 my-4' animationData={EmptyCartLotte} loop={true} />
-                        <Link
-                            to="/"
-                            className="px-3 py-3 lg:text-xl md:text-lg text-xs rounded-md bg-[#3DB188] text-white"
-                            >
-                            <button className="font-bold">Add books to cart</button>
-                        </Link> 
-                    </div>
-                    :
-                    <section className='lg:grid lg:grid-cols-8 flex md:flex-col flex-col-reverse sm:max-w-xl md:max-w-full lg:max-w-screen-xl lg:h-screen mx-auto'>
-                        <div className='col-span-6'>
-                
-                            <h1 className='text-2xl text-gray-700 font-semibold text-center md:py-4 py-10'>Your Items <span>{cartQuantity}</span></h1>
-                            <div>
-                            {
-                                data?.items?.map((item:any) => <ProductDetails key={item.id} {...item}></ProductDetails>)
-                            }
-                            </div>
-                        </div>
-                            <Summary />
-                    </section>
-
+        <section className='lg:grid lg:grid-cols-8 flex md:flex-col flex-col-reverse'>
+            <div className='col-span-6'>
+                <h1 className='text-2xl text-gray-700 font-semibold text-center md:py-4 py-10'>Your Items <span>{cartQuantity}</span></h1>
+                {
+                    cartItems.map((item) => <ProductDetails key={item.id} {...item}></ProductDetails>)
                 }
-            
-            
-        </>
+            </div>
+            <Summary />
+        </section>
     );
 };
 
--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,243 +1,93 @@
-import { useQuery } from "@tanstack/react-query"
-import { createContext, useContext, ReactNode, useState } from "react"
-import { toast } from "react-hot-toast"
-import { useLocalStorage } from "../hooks/useLocalStorage"
-import { AuthContext } from "./AuthProvider"
+import { createContext, useContext, ReactNode } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 type CartProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 type CartContext = {
-    getItemQuantity: (id: number) => number
-    increaseCartQuantity: (id: number) => void
-    decreaseCartQuantity: (id: number) => void
-    removeFromCart: (id: number) => void
-    cartQuantity: number
-    cartItems: CartItem[]
-}
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+};
 
 type CartItem = {
-    id: number
-    quantity: number
-}
+  id: number;
+  quantity: number;
+};
 
-const CartContext = createContext({} as CartContext)
+const CartContext = createContext({} as CartContext);
 
 export const useCart = () => {
-    return useContext(CartContext)
-}
+  return useContext(CartContext);
+};
 
 export const CartProvider = ({ children }: CartProviderProps) => {
-    const [cartItems, setCartItems] = useState<CartItem[]>([]);
-    // const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("cart", [])
-    const {user}=useContext(AuthContext);
-    const userEmail = user?.email;
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("cart", []);
 
-    const { data, isLoading, isError, refetch } = useQuery({
-        queryKey: ['cart', user?.email],
-        queryFn: async () => {
-            const res = await fetch(`http://localhost:5000/cart/${user?.email}`);
-            const data = await res.json();
-            return data;
-        }
-    })
-    
-      if (isLoading) {
-        return <div>Loading...</div>;
+  const cartQuantity = cartItems.reduce(
+    (quantity, item) => item.quantity + quantity,
+    0
+  );
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  };
+
+  const increaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id) == null) {
+        return [...currentItems, { id, quantity: 1 }];
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
       }
-    
-      if (isError) {
-        return <div>Error fetching cart data</div>;
+    });
+  };
+
+  const decreaseCartQuantity = (id: number) => {
+    setCartItems((currentItems) => {
+      if (currentItems.find((item) => item.id === id)?.quantity === 1) {
+        return currentItems.filter((item) => item.id !== id);
+      } else {
+        return currentItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
       }
+    });
+  };
 
-    const cartQuantity = data?.items?.reduce((quantity: number, item: CartItem) => item.quantity + quantity,0)
+  const removeFromCart = (id: number) => {
+    setCartItems((currentItems) => {
+      return currentItems.filter((item) => item.id !== id);
+    });
+  };
+  // console.log(cartItems);
 
-    const getItemQuantity = (id: number) => {
-        return cartItems.find((item: CartItem) => item.id === id)?.quantity || 0;
-    }
-    
-
-    const increaseCartQuantity = (id: number) => {
-        setCartItems(currentItems => {
-            if (currentItems.find(item => item.id === id) == null) {
-                return [...currentItems, { id, quantity: 1 }]
-            } else {
-                return currentItems.map(item => {
-                    if (item.id === id) {
-                    //   toast.success('Successfully added')
-                        return { ...item, quantity: item.quantity + 1 }
-                    } else {
-                        // add item to server
-                        // fetch('http://localhost:5000/add-to-cart', {
-                        //     method: 'POST',
-                        //     headers: {
-                        //         'content-type': 'application/json',
-                        //     },
-                        //     body: JSON.stringify({
-                        //                 id,
-                        //                 quantity: getItemQuantity(id),
-                        //                 userEmail
-                        //             })
-                        // })
-                        //     .then(res => res.json())
-                        //     .then(data => {
-                        //         console.log(data)
-                        //         if (data.acknowledged) {
-                        //             toast.success('Successfully added')
-                        //         }
-                        //     })
-                        //     .catch(e => console.error(e))
-                        return item
-                    }
-                })
-            }
-        })
-        console.log(getItemQuantity(id));
-        const cartData ={
-            id,
-            quantity: getItemQuantity(id),
-            userEmail
-        }
-        // add item to server
-        fetch('http://localhost:5000/add-to-cart', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(cartData)
-        })
-        .then(res => {
-            if (res.ok) {
-                toast.success('Successfully added')
-            } else {
-                throw new Error('Failed to add item to cart')
-            }
-        })
-        .catch(e => console.error(e))
-        // fetch('http://localhost:5000/add-to-cart', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         id,
-        //         quantity: getItemQuantity(id),
-        //         userEmail
-        //     })
-        // })
-        // .then(response => {
-        //     // Handle response from server here
-        //     if (!response.ok) {
-        //         throw new Error('Network response was not ok');
-        //     }
-        // })
-        // .then(data => {
-        //     // Refetch data from server after updating cart
-        //     refetch();
-        // })
-        // .catch(error => {
-        //     // Handle error here
-        //     console.error('Error sending data to server:', error);
-        // }); 
-    }
-
-    const decreaseCartQuantity = (id: number) => {
-        setCartItems(currentItems => {
-            if (currentItems.find(item => item.id === id)?.quantity === 1) {
-                return currentItems.filter(item => item.id !== id)
-            } else {
-                return currentItems.map(item => {
-                    if (item.id === id) {
-                    //   toast.success('Successfully removed')
-                        return { ...item, quantity: item.quantity - 1 }
-                    } else {
-                        return item
-                    }
-                })
-            }
-        })
-        const cartData ={
-            id,
-            quantity: getItemQuantity(id),
-            userEmail
-        }
-        // subtract item from server
-        fetch('http://localhost:5000/subtract-from-cart', {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify(cartData)
-        })
-        .then(res => {
-            if (res.ok) {
-                toast.success('Successfully removed')
-            } else {
-                throw new Error('Failed to add item to cart')
-            }
-        })
-        .catch(e => console.error(e))
-        // fetch('http://localhost:5000/subtract-from-cart', {
-        //     method: 'PUT',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         id,
-        //         quantity: getItemQuantity(id),
-        //         userEmail
-        //     })
-        // })
-        // .then(response => {
-        //     // Handle response from server here
-        //     if (!response.ok) {
-        //         throw new Error('Network response was not ok');
-        //       }
-        // })
-        // .then(data => {
-        //     // Refetch data from server after updating cart
-        //     refetch();
-        // })
-        // .catch(error => {
-        //     // Handle error here
-        //     console.error('Error sending data to server:', error);
-        // });
-    }
-
-    const removeFromCart = (id: number) => {
-        setCartItems(currentItems => {
-            return currentItems.filter(item => item.id !== id)
-        })
-        // remove item from server
-        // fetch(`http://localhost:5000/remove-from-cart/${id}/${userEmail}`, {
-        //     method: 'DELETE',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify({
-        //         userEmail
-        //     })
-        // })
-        // .then(response => {
-        //     // Handle response from server here
-        //     if (!response.ok) {
-        //         throw new Error('Network response was not ok');
-        //       }
-        // })
-        // .then(data => {
-        //     // Refetch data from server after updating cart
-        //     refetch();
-        // })
-        // .catch(error => {
-        //     // Handle error here
-        //     console.error('Error sending data to server:', error);
-        // });
-    }
-    
-    return (
-        <CartContext.Provider value={{ getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart, cartQuantity, cartItems }}>
-            {children}
-        </CartContext.Provider>
-    )
+  return (
+    <CartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+        cartItems,
+        cartQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
 }
