99f44dbfd883b9f88fe43fc87134210dba7536d0
MORSHED-HOME <morshed952640@gmail.com>
2023-02-26 01:35:57 +0600

    sidebar issue fixed in all books

--- src/layouts/Root/BooksLayout.tsx
+++ src/layouts/Root/BooksLayout.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import AllBooks from '../../Pages/AllBooks/AllBooks';
-import AllBooks2 from '../../Pages/AllBooks/AllBooks2';
 import NavBar from '../../Pages/Home/NavBar/NavBar';
 
 const BooksLayout = () => {
     return (
         <div>
             <NavBar />
-            <AllBooks2 />
+            <AllBooks />
         </div>
     );
 };
