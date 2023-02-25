dcef6b034339d626d28fdb7deb677eecccbeea3c
MORSHED-HOME <morshed952640@gmail.com>
2023-02-25 21:30:01 +0600

    home skelton fixed

--- src/Shared/Skeletons/SkeletonsHome.tsx
+++ src/Shared/Skeletons/SkeletonsHome.tsx
@@ -3,10 +3,12 @@ import Skeleton from './Skeleton';
 
 const SkeletonsHome = () => {
     return (
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 m-5 lg:p-5'>
-            {
-                [...Array(4)].map(skltn => <Skeleton />)
-            }
+        <div className='max-w-7xl mx-auto'>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-12 m-5 lg:p-5'>
+                {
+                    [...Array(4)].map(skltn => <Skeleton />)
+                }
+            </div>
         </div>
     );
 };
